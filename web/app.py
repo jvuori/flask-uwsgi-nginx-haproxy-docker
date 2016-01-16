717af4ac56b24e4f4cbb51ab751cce068fbc91e0
Jaakko Vuori <jaakko.vuori@gmail.com>
2016-01-16 18:34:23 +0200

    Print host name to investigate nodes behind LB

--- web/app.py
+++ web/app.py
@@ -1,11 +1,12 @@
 import os
+import socket
 
 from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World! (PID=%s)" % (os.getpid())
+    return "Hello World! (HOSTNAME=%s, PID=%s)" % (socket.gethostname(), os.getpid())
 
 if __name__ == "__main__":
     app.run()
