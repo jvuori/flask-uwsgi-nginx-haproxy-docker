debe6fa33065c8a1486eb4b64697b9332a148438
Jaakko Vuori <jaakko.vuori@gmail.com>
2016-01-16 18:34:40 +0200

    Simulate slowness with 0.1 sec sleep

--- web/app.py
+++ web/app.py
@@ -1,11 +1,13 @@
 import os
 import socket
+import time
 
 from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
+    time.sleep(0.1)
     return "Hello World! (HOSTNAME=%s, PID=%s)" % (socket.gethostname(), os.getpid())
 
 if __name__ == "__main__":
