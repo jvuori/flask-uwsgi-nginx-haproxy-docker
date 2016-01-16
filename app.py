e1715361077f1864edd3af9e182d29d39eb8c843
Jaakko Vuori <jaakko.vuori@gmail.com>
2016-01-16 13:33:42 +0200

    Print PID for investigating multi-process hosting

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
+import os
+
 from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "Hello World! (PID=%s)" % (os.getpid())
 
 if __name__ == "__main__":
     app.run()
